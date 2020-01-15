79dae683afcc42e3386bbdb68edae89430684308
Anthony <anthonyd-m@live.com>
2020-01-15 12:43:27 +0100

    created home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 export interface HomeProps {
-    
+    user_is_logged: boolean
+    user_role: string
 }
  
 export interface HomeState {
@@ -14,10 +17,25 @@ class Home extends React.Component<HomeProps, HomeState> {
         this.state = {};
     }
     render() { 
-        return (
-            <div></div>
-        );
+        if (this.props.user_is_logged) {
+            if (this.props.user_role == "client") {
+                return (<Redirect to="/doctors"></Redirect>);
+            }
+            else {
+                return (<Redirect to="/availabilities"></Redirect>)
+            }
+        }
+        else {
+            return (<Redirect to="/login"></Redirect>)
+        }
     }
 }
- 
-export default Home;
+
+let mapStateToProps = (state) => {
+    return {
+        user_is_logged: state.user_is_logged,
+        user_role: state.user_role
+    }
+}
+
+export default connect(mapStateToProps)(Home);
